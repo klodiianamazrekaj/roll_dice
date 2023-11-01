4b342ccffce7a31f312a4338e3da8d2b918a1abb
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-11-01 01:43:13 +0100

    main.dart changes

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 234, 184, 118),
+      body: Center(child: Text('hEY mAC')),
+    ),
+  ));
+}
