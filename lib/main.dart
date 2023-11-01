61c6597bb222e3c336caaeabdbe872c330712933
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-11-01 16:38:06 +0100

    minor changes made to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 234, 184, 118),
-      body: Center(child: Text('hEY mAC')),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 77, 206, 144), Colors.lightGreen],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Flutter',
+            style: TextStyle(color: Colors.black, fontSize: 50,),
+            ),
+        ),
+      ),
     ),
   ));
 }
